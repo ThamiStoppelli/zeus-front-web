364d2783a2a7cef4f609519f8e5bb2a1ec4747da
THAMIRES STOPPELLI <thamires@L65071282.acad.unifor.br>
2022-04-29 18:35:28 -0300

    second commit

--- src/pages/Form/index.jsx
+++ src/pages/Form/index.jsx
@@ -1,18 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from "../../components/Header";
 import { Container } from './style';
+import axios from 'axios'
+
+const url = "http://localhost:5000"
 
 const Form = () => {
 
   function newData() {
-    return{
-       name:'',
-       price: 0,
-       quantity:0,
-       description:''
-
+    return {
+      name: '',
+      price: 0,
+      amount: 0,
+      description: ''
     }
- }
+  }
 
   // const [title, setTitle] = useState('')
   //<input onChange={event => setTitle(event.target.value)} />
@@ -21,56 +23,68 @@ const Form = () => {
 
   const [data, setData] = useState(newData);
   const [print, setPrint] = useState(false);
-  
-  function getData(name)
-  {
+
+
+  function getData(name) {
     setData(name.target.value)
     setPrint(false)
-    console.warn(name.target.value)
+    // console.warn(name.target.value)
+    console.log(name.target.value)
   }
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value })
+    console.log(e.target.value)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    axios.post(`${url}/food/create`, data).then(function (response) { 
+      setData(response.data)
+    })
+      .catch(function (error) {
+        console.log(error); // Network Error
+        console.log(error.status); // undefined
+        console.log(error.code); // undefined
+      });
+  }
+
 
   return (
     <>
 
-    <Container>
-      <Header />
-      <h2>Cadastrar ração</h2>
-      <form>
-        <label>
-          Marca da ração:
- 
-          {
-            print?
-            <h2>{data}</h2>
-            :null
-          }
-          <input placeholder='Digite o nome da marca da ração' onChange={event => getData(event.target.value)} />
-          <button onClick={()=>setPrint(true)}>Imprimir</button>
+      <Container>
+        <Header />
+        <h2>Cadastrar ração</h2>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Marca da ração:
+            <input type="text" name="name" value={data.name} placeholder='Nome da marca' onChange={handleChange} />
+          </label>
+          <label>
+            Preço (R$):
+            <input type="number" name="price" value={data.price} placeholder='Preço em reais' onChange={handleChange} />
+          </label>
+          <label>
+            Quantidade (kg):
+            <input type="number" name="amount" value={data.amount} placeholder='Quantidade em quilogramas' onChange={handleChange} />
+          </label>
+          <label>
+            Descrição:
+            <input type="text" name="description" value={data.description} placeholder='' onChange={handleChange} />
+          </label>
 
-        </label>
-        <label>
-          Preço (R$):
-          <input placeholder='Digite o preço em reais' />
-        </label>
-        <label>
-          Quantidade(kg):
-          <input placeholder='Digite a quantidade em quilogramas' />
-        </label>
-        <label>
-          Descrição:
-          <input placeholder='' />
-        </label>
-        
-        {/* button */}
-        <input type="submit" value="Cadastrar"></input> 
+          {/* button */}
+          <input type="submit" value="Cadastrar"></input>
 
-      </form>
+        </form>
 
-      <p>Você clicou {count} vezes</p>
-        <button onClick={() => setCount(count + 1)}>
+        <p>Você clicou {count / 2} vezes</p>
+        <button onClick={() => setCount(count + 2)}>
           Clique aqui
         </button>
-    </Container>
+      </Container>
     </>
   );
 }
